1e426ffb7127dc9207a88d42230b217ce20a3a13
ELCOT <ELCOT@Lenovo>
2024-09-02 09:41:14 -0800

    commit1

---
+++ C43_Gayathri/src/com/tnsif_day01/MyfirstProgram/MyfirstProgram.java
@@ -0,0 +1,11 @@
+package com.tnsif_day01.MyfirstProgram;
+
+public class MyfirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Gayu");
+	}
+}
+
+
+
