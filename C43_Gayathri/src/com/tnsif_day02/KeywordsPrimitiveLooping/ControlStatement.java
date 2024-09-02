3e025d54b189a7935012fc187fc936f35d6d2513
ELCOT <ELCOT@Lenovo>
2024-09-02 09:50:03 -0800

    commit1

---
+++ C43_Gayathri/src/com/tnsif_day02/KeywordsPrimitiveLooping/ControlStatement.java
@@ -0,0 +1,18 @@
+package com.tnsif_day02.KeywordsPrimitiveLooping;
+
+public class ControlStatement {
+	public static void main(String[] args) {
+		int age=11;
+		
+		if(age>=18)
+			System.out.println(" You are eligible for Voting");	
+		else
+			System.out.println("Sorry! You need to wait more");
+
+		System.out.println("if else demonstration");
+		
+	}
+
+}
+
+
